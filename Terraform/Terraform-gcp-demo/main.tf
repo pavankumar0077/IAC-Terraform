223d93565e434b75627b1f301be95d0f702540a6
Pavan Kumar Dasari <40380941+pavankumar0077@users.noreply.github.com>
2024-09-10 17:49:31 +0530

    Add files via upload

---
+++ Terraform/Terraform-gcp-demo/main.tf
@@ -0,0 +1,30 @@
+ provider "google" {
+     credentials = file("gcp-account.json")
+     project     = "terraform-431112"
+     region      = "europe-west4"
+     zone        = "europe-west4-a"
+}
+
+
+# main.tf
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # To keep the setup simple you can set the network_interface to default
+  # For Advance network setup refer to Point-7 : Setup Network and Firewall for virtual machine
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
